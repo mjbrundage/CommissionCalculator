56451de26c5d21275f74c3195e94d0741e63b8ce
Matt Brundage II <mattjbrundage@icloud.com>
2021-03-02 12:01:52 -0500

    Created NumbersDemo
    Simple Java Program

---
+++ NumbersDemo/NumbersDemo.java
@@ -0,0 +1,32 @@
+package NumbersDemo;
+// Filename: NumberDemo.java
+// Programmer: Matt Brundage II
+// Date: 3/1/21
+
+public class NumbersDemo {
+    public static void main (String args[]) {
+        int myNum1 = 4;
+        int myNum2 = 33;
+        displayTwiceTheNumber(myNum1);
+        displayNumberPlusFive(myNum1);
+        displayNumberSquared(myNum1);
+        displayTwiceTheNumber(myNum2);
+        displayNumberPlusFive(myNum2);
+        displayNumberSquared(myNum2);
+    }
+    public static void displayTwiceTheNumber(int n) {
+        int total;
+        total = n * 2;
+        System.out.println(n + " times 2 is " + total);
+    }
+    public static void displayNumberPlusFive(int n) {
+        int total;
+        total = n + 5;
+        System.out.println(n + " plus 5 is " + total);
+    }
+    public static void displayNumberSquared(int n) {
+        int total;
+        total = n * n;
+        System.out.println(n + " squared is " + total);
+    }
+}
---
+++ NumbersDemo/README.md
@@ -0,0 +1,5 @@
+# NumbersDemo
+
+## Simple Java Program
+
+This program demonstrates calling a method multiple times in Java code.
