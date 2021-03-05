174f875d0664e7560f9eaff5292fbee09d297d10
Matt Brundage II <mattjbrundage@icloud.com>
2021-03-05 11:41:20 -0500

    Updates to IPO and Code
    Nearly Complete IPO and continued Code.

--- CommissionCalculator/CommissionCalculator.cpp
+++ CommissionCalculator/CommissionCalculator.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
-    
+    //Declare Variables
+    double quarterSales = 0.0;
+    double commissionAmt = 0.0;
+
+    //Receive Input
+    cout << "Quarterly Sales: ";
+    cin >> quarterSales;
 }
